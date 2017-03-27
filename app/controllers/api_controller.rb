9f02e6298513abe2d2110c2eb8ba78e629351b4f
Rah Zhou <youz@ualberta.ca>
2017-03-27 15:28:05 -0700

    implemented PDF template

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,17 +1,33 @@
 class ApiController < ApplicationController
   # before_action :set_itinerary, only: [:show, :calendar]
+  protect_from_forgery with: :null_session
 
   def cv
-    id = 1
-    tex_content = render_to_string("cv.tex", layout: false)
-    tex_file_name = "./tmp/member_#{id}_cv.tex"
-    File.open(tex_file_name, 'w') do |f|
-      f.puts tex_content
+    if params[:data].blank?
+      render text: "Sorry the data is corrupted, please try again."
+    else
+      begin
+        @data = JSON.parse Base64.decode64(params[:data])
+        @data.symbolize_keys!
+
+        tex_content = render_to_string("cv.tex", layout: false)
+        
+        tex_file_name = "./tmp/member_#{ @data[:user_id] }_cv.tex"
+        File.open(tex_file_name, 'w') do |f|
+          f.puts tex_content
+        end
+        
+        result = `pdflatex -halt-on-error -output-directory=tmp #{ tex_file_name }`
+        output_file_name = "./tmp/member_#{ @data[:user_id] }_cv.pdf"
+        send_file output_file_name, type: "application/pdf", disposition: 'inline'  
+
+      rescue
+        render text: "Sorry there's an error processing the PDF file. Please try again."
+      end
+      
     end
+
     
-    result = `pdflatex -halt-on-error -output-directory=tmp #{tex_file_name}`
-    output_file_name = "./tmp/member_#{id}_cv.pdf"
-    send_file output_file_name, type: "application/pdf", disposition: 'inline'
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # escape special characters to make the string safe in LaTex script
+  def escaple_latex(str)
+    if str.blank?
+      ""
+    else
+      str.gsub(/([\&\#\_])/,"\\\\\\1")
+    end
+  end
 end
